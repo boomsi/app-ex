7a56e1331e4024e696644825cdb9d0a8e4c93711
boomsi <libo19431@gmail.com>
2023-06-08 16:00:01 +0800

    fix: nested

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,9 +1,12 @@
+import 'package:app/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:app/type.dart';
 
 class ListPage extends StatefulWidget implements HasLayoutGroup {
-  ListPage({Key? key, required this.layoutGroup, required this.onLayoutToggle})
+  const ListPage(
+      {Key? key, required this.layoutGroup, required this.onLayoutToggle})
       : super(key: key);
+
   final LayoutGroup layoutGroup;
   final VoidCallback onLayoutToggle;
 
@@ -12,8 +15,71 @@ class ListPage extends StatefulWidget implements HasLayoutGroup {
 }
 
 class _ListPageState extends State<ListPage> {
+  Widget _buildContent() {
+    return ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, index) {
+          Contact contact = list[index];
+          return ContactListTitle(contact: contact);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('ListPage'));
+    return Scaffold(
+      appBar: MainAppBar(
+        layoutGroup: widget.layoutGroup,
+        onLayoutToggle: widget.onLayoutToggle,
+        layoutType: LayoutType.list,
+      ),
+      body: Container(
+        child: _buildContent(),
+      ),
+    );
   }
 }
+
+class ContactListTitle extends ListTile {
+  ContactListTitle({Key? key, required Contact contact})
+      : super(
+            key: key,
+            title: Text(contact.name),
+            subtitle: Text(contact.email),
+            leading: CircleAvatar(
+              child: Text(contact.name[0]),
+            ));
+}
+
+class Contact {
+  final String name;
+  final String email;
+
+  Contact({required this.name, required this.email});
+}
+
+List<Contact> list = [
+  Contact(name: 'Aaryan', email: 'dsa@das.com'),
+  Contact(name: 'Isa Tusa', email: 'isa.tusa@me.com'),
+  Contact(name: 'Racquel Ricciardi', email: 'racquel.ricciardi@me.com'),
+  Contact(name: 'Teresita Mccubbin', email: 'teresita.mccubbin@me.com'),
+  Contact(name: 'Rhoda Hassinger', email: 'rhoda.hassinger@me.com'),
+  Contact(name: 'Carson Cupps', email: 'carson.cupps@me.com'),
+  Contact(name: 'Aaryan', email: 'dsa@das.com'),
+  Contact(name: 'Isa Tusa', email: 'isa.tusa@me.com'),
+  Contact(name: 'Racquel Ricciardi', email: 'racquel.ricciardi@me.com'),
+  Contact(name: 'Teresita Mccubbin', email: 'teresita.mccubbin@me.com'),
+  Contact(name: 'Rhoda Hassinger', email: 'rhoda.hassinger@me.com'),
+  Contact(name: 'Carson Cupps', email: 'carson.cupps@me.com'),
+  Contact(name: 'Aaryan', email: 'dsa@das.com'),
+  Contact(name: 'Isa Tusa', email: 'isa.tusa@me.com'),
+  Contact(name: 'Racquel Ricciardi', email: 'racquel.ricciardi@me.com'),
+  Contact(name: 'Teresita Mccubbin', email: 'teresita.mccubbin@me.com'),
+  Contact(name: 'Rhoda Hassinger', email: 'rhoda.hassinger@me.com'),
+  Contact(name: 'Carson Cupps', email: 'carson.cupps@me.com'),
+  Contact(name: 'Aaryan', email: 'dsa@das.com'),
+  Contact(name: 'Isa Tusa', email: 'isa.tusa@me.com'),
+  Contact(name: 'Racquel Ricciardi', email: 'racquel.ricciardi@me.com'),
+  Contact(name: 'Teresita Mccubbin', email: 'teresita.mccubbin@me.com'),
+  Contact(name: 'Rhoda Hassinger', email: 'rhoda.hassinger@me.com'),
+  Contact(name: 'Carson Cupps', email: 'carson.cupps@me.com'),
+];
--- lib/pages/nested_page.dart
+++ lib/pages/nested_page.dart
@@ -1,8 +1,9 @@
+import 'package:app/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:app/type.dart';
 
 class NestedPage extends StatefulWidget implements HasLayoutGroup {
-  NestedPage(
+  const NestedPage(
       {Key? key, required this.layoutGroup, required this.onLayoutToggle})
       : super(key: key);
 
@@ -14,10 +15,71 @@ class NestedPage extends StatefulWidget implements HasLayoutGroup {
 }
 
 class _NestedPageState extends State<NestedPage> {
+  Widget _buildContent() {
+    return ListView.builder(
+      itemCount: 16,
+      itemBuilder: (BuildContext context, index) {
+        return _buildHorizontalList(pageIndex: index);
+      },
+    );
+  }
+
+  Widget _buildHorizontalList({required int pageIndex}) {
+    final List<Color> colors = [
+      Colors.green,
+      Colors.blue,
+      Colors.red,
+      Colors.yellow,
+      Colors.orange,
+      Colors.purple,
+    ];
+    double height = 136;
+    return SizedBox(
+        height: height,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 20,
+          itemBuilder: (BuildContext context, int index) {
+            return _buildItem(
+                index: index + 1,
+                color: colors[(pageIndex + index) % colors.length],
+                parentSize: height);
+          },
+        ));
+  }
+
+  Widget _buildItem(
+      {required int index, required Color color, required double parentSize}) {
+    double edgeSize = 8;
+    double itemSize = parentSize - edgeSize * 2;
+    return Container(
+      padding: EdgeInsets.all(edgeSize),
+      child: SizedBox(
+        width: itemSize,
+        height: itemSize,
+        child: Container(
+          alignment: AlignmentDirectional.center,
+          color: color,
+          child: Text(
+            '$index',
+            style: const TextStyle(fontSize: 72, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('NestedPage'),
+    return Scaffold(
+      appBar: MainAppBar(
+        layoutGroup: widget.layoutGroup,
+        onLayoutToggle: widget.onLayoutToggle,
+        layoutType: LayoutType.nested,
+      ),
+      body: Container(
+        child: _buildContent(),
+      ),
     );
   }
 }
